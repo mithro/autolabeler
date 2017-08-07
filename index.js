2f9b7d79756dbc46b85bd7eb62e328557dd22d2a
Luis Schubert <schubert.luis@gmail.com>
2017-08-07 15:21:31 -0700

    apparently foreach is not a function

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ module.exports = robot => {
             owner: 'luisschubert',
             repo: 'webhookTest'
         });
+        console.log(PRs);
         //for pr in PRS check what labels should be there.
         PRs.forEach(async function(PR){
             console.log("github is: "+github);

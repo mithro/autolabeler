b47de894379afc5ee5f43d8833d6b5507d79614b
Greg Lockwood <greg.lockwood.is@gmail.com>
2019-07-26 10:17:21 -0700

    First attempt

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const yaml = require('js-yaml')
-const ignore = require('ignore')
+const yaml = require('js-yaml');
 
 module.exports = robot => {
-  robot.on('pull_request.opened', autolabel)
-  robot.on('pull_request.synchronize', autolabel)
+  robot.on('pull_request.opened', sizelabel);
+  robot.on('pull_request.synchronize', sizelabel);
 
-  async function autolabel (context) {
-    const content = await context.github.repos.getContent(context.repo({
-      path: '.github/autolabeler.yml'
-    }))
-    const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
+  async function sizelabel (context) {
+    const content = await context.github.repos.getContents(context.repo({
+      path: '.github/sizelabeler.yml'
+    }));
+    const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString());
 
-    const files = await context.github.pullRequests.getFiles(context.issue())
-    const changedFiles = files.data.map(file => file.filename)
+    const {additions, deletions} = await context.github.pullRequests.get(context.issue());
+    const netSize = additions - deletions;
 
-    const labels = new Set()
+    const labels = new Set();
 
     // eslint-disable-next-line guard-for-in
     for (const label in config) {
-      robot.log('looking for changes', label, config[label])
-      const matcher = ignore().add(config[label])
-
-      if (changedFiles.find(file => matcher.ignores(file))) {
+      robot.log('looking for size matches', label, config[label]);
+      let passes = false;
+      if ('min' in config[label]) {
+        passes = passes && netSize >= config[label].min;
+      }
+      if ('max' in config[label]) {
+        passes = passes && netSize <= max;
+      }
+      if (passes) {
         labels.add(label)
       }
     }

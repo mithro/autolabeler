53d4089c5a43731597e3abd22bdde52e8481e52a
Gabitchov <gabriel.pascual@gmail.com>
2020-11-20 08:50:09 +0100

    chore: upgrade framework
      - upgrade node version
      - add precommit for standard

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const yaml = require("js-yaml");
-const ignore = require("ignore");
+const yaml = require('js-yaml')
+const ignore = require('ignore')
 
 module.exports = ({ app }) => {
-  app.on("pull_request.opened", autolabel);
-  app.on("pull_request.synchronize", autolabel);
+  app.on('pull_request.opened', autolabel)
+  app.on('pull_request.synchronize', autolabel)
 
-  async function autolabel(context) {
+  async function autolabel (context) {
     const content = await context.octokit.repos.getContent(
       context.repo({
-        path: ".github/autolabeler.yml",
+        path: '.github/autolabeler.yml'
       })
-    );
+    )
     const config = yaml.safeLoad(
-      Buffer.from(content.data.content, "base64").toString()
-    );
+      Buffer.from(content.data.content, 'base64').toString()
+    )
 
-    const files = await context.octokit.pulls.listFiles(context.issue());
-    const changedFiles = files.data.map((file) => file.filename);
+    const files = await context.octokit.pulls.listFiles(context.issue())
+    const changedFiles = files.data.map((file) => file.filename)
 
-    const labels = new Set();
+    const labels = new Set()
 
     // eslint-disable-next-line guard-for-in
     for (const label in config) {
-      app.log("looking for changes", label, config[label]);
-      const matcher = ignore().add(config[label]);
+      app.log('looking for changes', label, config[label])
+      const matcher = ignore().add(config[label])
 
       if (changedFiles.find((file) => matcher.ignores(file))) {
-        labels.add(label);
+        labels.add(label)
       }
     }
 
-    const labelsToAdd = Array.from(labels);
+    const labelsToAdd = Array.from(labels)
 
-    app.log("Adding labels", labelsToAdd);
+    app.log('Adding labels', labelsToAdd)
     if (labelsToAdd.length > 0) {
       return context.octokit.issues.addLabels(
         context.issue({
-          labels: labelsToAdd,
+          labels: labelsToAdd
         })
-      );
+      )
     }
   }
-};
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,56 +1,57 @@
-const { Probot, ProbotOctokit } = require("probot");
-const nock = require("nock");
-const fs = require("fs");
-const plugin = require("..");
+const { Probot, ProbotOctokit } = require('probot')
+const nock = require('nock')
+const plugin = require('..')
 
 const config = `
 test: test*
 config: .github
 frontend: ["*.js"]
-`;
+`
 
-describe("autolabeler", () => {
-  let probot;
+describe('autolabeler', () => {
+  let probot
 
   beforeEach(() => {
-    nock.disableNetConnect();
+    nock.disableNetConnect()
     probot = new Probot({
       id: 1,
-      githubToken: "test",
+      githubToken: 'test',
       // Disable throttling & retrying requests for easier testing
       Octokit: ProbotOctokit.defaults({
         retry: { enabled: false },
-        throttle: { enabled: false },
-      }),
-    });
-    probot.load(plugin);
-  });
+        throttle: { enabled: false }
+      })
+    })
+    probot.load(plugin)
+  })
 
   afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
+    nock.cleanAll()
+    nock.enableNetConnect()
+  })
 
-  describe("pull_request.opened event", () => {
-    const event = require("./fixtures/pull_request.opened");
+  describe('pull_request.opened event', () => {
+    const event = require('./fixtures/pull_request.opened')
 
-    test("adds label", async () => {
+    test('adds label', async () => {
       // Test that we correctly return a test token
-      nock("https://api.github.com")
-        .get("/repos/robotland/test/contents/.github%2Fautolabeler.yml")
-        .reply(200, { content: btoa(config) })
-        .get("/repos/robotland/test/pulls//files?issue_number=98")
-        .reply(200, [
-          { filename: "test.txt" },
-          { filename: ".github/autolabeler.yml" },
-        ])
-        .post("/repos/robotland/test/issues/98/labels", (body) => {
-          expect(body).toEqual(["test", "config"]);
-          return true;
+      nock('https://api.github.com')
+        .get('/repos/robotland/test/contents/.github%2Fautolabeler.yml')
+        .reply(200, {
+          content: Buffer.from(config, 'utf-8').toString('base64')
         })
-        .reply(200);
+        .get('/repos/robotland/test/pulls//files?issue_number=98')
+        .reply(200, [
+          { filename: 'test.txt' },
+          { filename: '.github/autolabeler.yml' }
+        ])
+        .post('/repos/robotland/test/issues/98/labels', (body) => {
+          expect(body).toEqual(['test', 'config'])
+          return true
+        })
+        .reply(200)
 
-      await probot.receive(event);
-    });
-  });
-});
+      await probot.receive(event)
+    })
+  })
+})
